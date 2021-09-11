60c9237506815c687a3870361841c0a46f1bf684
NikolayevAS <sasha.nickolaev2013@yandex.ru>
2021-09-11 14:50:12 +0300

    add print

--- Lab1.py
+++ Lab1.py
@@ -1,2 +1,95 @@
-print ('Hello')
+import math as m
+import main
 
+from math import sqrt as s
+
+# main.print_hi()
+# m.pi
+# s()
+
+#
+# # объявил переменную и присвоил ей значение
+# value = 10.2
+#
+# studentName = 'Alex'
+#
+# age = 21
+#
+# sum1 = age > value
+#
+# age2 = float(age)
+#
+# s = list()
+# s1 = ["M" , "E"]
+# s1.append("I")
+# s2 = s1[2]
+#
+#
+# s2 = s1[2]
+#
+# s3 = tuple()
+# s3 = (1, 2)
+# f = s3[1]
+#
+# d = dict()
+#
+# d = {1 : 'Vasya'}
+# d = {2 : 'Semen'}
+#
+# a = d.get(2)
+#
+# s = set()
+#
+# # множество
+# s.add(1)
+# s.add(2)
+# s.add(2)
+#
+# sss = True
+#
+# if value > age:
+#     print("i'm here!")
+#
+# print('fdsq')
+#
+# if True:
+#     print('fdsf')
+# elif sss:
+#     print(sss)
+# else:
+#     print('Не прошли проверки')
+#
+# tttt = True
+# i = 0
+# # while tttt:
+# #     print("aaaaa")
+# #     i += 1
+# #     if i > 10:
+# #         tttt = False
+#
+# for i in s1:
+#     print(i)
+
+# val0 = 10
+# val1 = 0
+#
+# def yOtX():
+#     val1 = val0
+#     return val1
+#
+# #yOtX()
+# print(yOtX())
+# print(val1)
+
+# for letter in 'лучшийggggggt':
+#     if letter == 'g':
+#         continue
+#     elif letter == 'a':
+#         break
+#     print(letter)
+
+# str2 = 'Я love МЭИ'
+#
+# str1 = 'по субботам'
+#
+# print(str2 + ' ' + str1 + ' ' + str(value) + ' раз')
